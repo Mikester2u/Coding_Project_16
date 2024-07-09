7aeaa1a5af8e8d500a09b854d28c0bb61001f3c5
Michael Rueda <michael.a.rueda22@gmail.com>
2024-07-09 17:09:51 -0400

    push

--- my-teams-app/src/App.js
+++ my-teams-app/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+//U22562170
+
+import React from 'react';
 import './App.css';
+
+const teamData = [
+  {name:'Michael Rueda', role:'Manager', photo:'path/to/Michael.jpg'},
+  {name:'Jeff Bezos', role:'Janitor', photo:'path/to/Jeff.jpg'},
+  {name:'Lebron James', role:'King', photo:'path/to/Lebron.jpg'},
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header title="Meet the Team" />
+    <Gallery team={teamData} />
+  </div>
+  );
+}
+
+function Header({title}) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+function Profile({ name, role, photo }) {
+  return (
+    <div className="profile">
+      <img src={photo} alt={`${name}'s photo`} />
+      <h2>{name}</h2>
+      <p>{role}</p>
+    </div>
+  );
+}
+
+function Gallery({ team }) {
+  return (
+    <div className="gallery">
+      {team.map((member, index) => (
+        <Profile key={index} {...member} />
+      ))}
     </div>
   );
 }
